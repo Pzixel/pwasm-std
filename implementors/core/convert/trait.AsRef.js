2dc5d833b3a11d07d915133d64c96c718069054c
Travis CI User <travis@example.org>
2018-01-15 12:24:05 +0000

    Update documentation

---
+++ implementors/core/convert/trait.AsRef.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["pwasm_std"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[u8]&gt; for <a class=\"struct\" href=\"pwasm_std/struct.WrappedArgs.html\" title=\"struct pwasm_std::WrappedArgs\">WrappedArgs</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["pwasm_std"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"pwasm_std/struct.WrappedArgs.html\" title=\"struct pwasm_std::WrappedArgs\">WrappedArgs</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
